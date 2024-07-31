5c7ad1120b06525a6929ff02107c282be8f37697
Levick <anirjeeth@gmail.com>
2024-07-31 23:55:39 +0530

    add: path changes

--- src/Mid.jsx
+++ src/Mid.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Mid.css';
+import aboutme from "./src/Markdownjsx/aboutme.jpg";
+import meme from "./src/Markdownjsx/image-2.png";
+import moon from "./src/Markdownjsx/full-moon.jpg";
+import vimb from "./src/Markdownjsx/vimB.png";
 
 const blogPosts = [
-  { date: "July 31, 2024", title: "About", description: "💙..About..em..what?", link: "post2", year: 2024, bgImage: "src/Markdownjsx/aboutme.jpg" },
-  { date: "February 22, 2024", title: "Docker Essentials: Navigating the Container Seas", description: "Docker and its Best Practices 🐳", link: "post3", year: 2024, bgImage: "src/Markdownjsx/image-2.png" },
-  { date: "December 31, 2023", title: "Astrophotography", description: "Various photos taken through my camera this year", link: "post4", year: 2023, bgImage: "src/Markdownjsx/full-moon.jpg" },
-  { date: "December 24, 2023", title: "Learning Vim...?", description: "Beginner guide to get started with vim", link: "post5", year: 2023, bgImage: "src/Markdownjsx/vimB.png" }
+  { date: "July 31, 2024", title: "About", description: "💙..About..em..what?", link: "post2", year: 2024, bgImage: {aboutme} },
+  { date: "February 22, 2024", title: "Docker Essentials: Navigating the Container Seas", description: "Docker and its Best Practices 🐳", link: "post3", year: 2024, bgImage: {meme} },
+  { date: "December 31, 2023", title: "Astrophotography", description: "Various photos taken through my camera this year", link: "post4", year: 2023, bgImage: {moon} },
+  { date: "December 24, 2023", title: "Learning Vim...?", description: "Beginner guide to get started with vim", link: "post5", year: 2023, bgImage: {vimb} }
 ];
 
 const Mid = () => {
